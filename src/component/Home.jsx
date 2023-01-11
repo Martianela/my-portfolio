93514b41e30753e47f2cc079f613a689c38140f8
Martianela <97327719+Martianela@users.noreply.github.com>
2023-01-11 17:53:53 +0530

    Update Home.jsx
    Link removed

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
-import { Link } from "react-scroll";
+
 const Home = () => {
   return (
     <div name="home" className="w-full h-screen bg-[#0a192f]">
